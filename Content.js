fb575a29550329131e2922b98b5d4c08621cab0b
Sharvil <karwasharvil@gmail.com>
2022-09-19 17:33:19 +0530

    first commit

---
+++ Content.js
@@ -0,0 +1,17 @@
+switch (window.location.hostname) {
+  case "www.youtube.com":
+    document.body.innerHTML = "Studying>Youtube";
+    break;
+  case "www.facebook.com":
+    document.body.innerHTML = "Studying>Facebook";
+    break;
+  case "www.netflix.com":
+    document.body.innerHTML = "Studying>Netflix";
+    break;
+  case "www.instagram.com":
+    document.body.innerHTML = "Studying>Instagram";
+    break;
+  case "discord.com":
+    document.body.innerHTML = "Studying>Discord";
+    break;
+}
